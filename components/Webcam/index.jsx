46a6ffdc53188bd4819c78d59e4fbe4f7c65fca6
samuelochaba <109516201+samuelochaba@users.noreply.github.com>
2022-09-26 14:24:06 +0100

    fix

--- components/Webcam/index.jsx
+++ components/Webcam/index.jsx
@@ -18,7 +18,7 @@ const Camera = ({ constraints, uploadToS3AndExtract }) => {
       <Webcam
         audio={false}
         screenshotFormat="image/jpeg"
-        className="h-[150px]    mx-auto rounded-tl-lg rounded-tr-lg mt-5"
+        className="h-[40vh]    mx-auto rounded-tl-lg rounded-tr-lg mt-5"
         videoConstraints={constraints}
       >
         {
@@ -37,7 +37,7 @@ const Camera = ({ constraints, uploadToS3AndExtract }) => {
       <Webcam
         audio={false}
         screenshotFormat="image/jpeg"
-        className="h-[150px] mx-auto rounded-bl-lg rounded-br-lg"
+        className="h-[40vh] mx-auto rounded-bl-lg rounded-br-lg"
         videoConstraints={constraints}
       >
         {({ getScreenshot }) => (
